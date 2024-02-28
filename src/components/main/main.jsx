3e90bf9a55c978b920cf2aa8c4979b3b44c4d27e
Никита Апатенко <119950786+Beinyc@users.noreply.github.com>
2024-02-28 13:07:30 +0300

    0.6.2.2
    Исправлен баг при ктором все дивы схлопывались

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -5,10 +5,14 @@ import mainImage from '../../images/main.jpg'
 export default function Main() {
     return (
         <div className='main'>
-           <p className='main__text'>Мы приглашаем вас на нашу свадьбу</p>
-           <img src={mainImage} alt="фото пары"  className='main__image'/>
-           <p className='main__paragraph'>Будем счастливы с вами разделить этот день вместе с вами</p>
-           <p className='main__date'>29.03.2024</p> 
+           <div className='main__flex-text'>
+                <p className='main__text'>Мы приглашаем вас на нашу свадьбу</p>
+                <p className='main__paragraph'>Будем счастливы с вами разделить этот день вместе с вами</p>
+                <p className='main__date'>29.03.2024</p> 
+           </div>
+           <div className='main__flex-image'>
+                <img src={mainImage} alt="фото пары"  className='main__image'/>
+           </div>
         </div>
     );
 };
