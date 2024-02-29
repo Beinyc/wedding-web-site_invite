7ab0fe6b7f65761a3c36769e40cffd39a93f181a
Никита Апатенко <119950786+Beinyc@users.noreply.github.com>
2024-03-01 00:15:17 +0300

    v 0.6.4
    готов адаптивный блок с программой

--- src/components/program/program.jsx
+++ src/components/program/program.jsx
@@ -6,38 +6,40 @@ export default function Program() {
     return (
         <div className='program'>
             <img className='program__vector' src={vector} alt="дуга" />
-            <p className='program__title'>Программа дня</p>
-            <div className='program__list-flex'>
-                <div className='program__time-flex'>
-                    <p className='program__time'>16:30</p>
-                    <p className='program__dash'>—</p>
-                    <div className='program__item-flex'>
-                        <p className='program__margin-zero program__paragraph-up'>Сбор гостей</p>
-                        <p className='program__margin-zero program__paragraph-down'>Парк Зеленый Лог</p>
+            <div className='program__flex'>
+                <p className='program__title'>Программа дня</p>
+                    <div className='program__list-flex'>
+                        <div className='program__time-flex'>
+                            <p className='program__time'>16:30</p>
+                            <p className='program__dash'>—</p>
+                            <div className='program__item-flex'>
+                                <p className='program__margin-zero program__paragraph-up'>Сбор гостей</p>
+                                <p className='program__margin-zero program__paragraph-down'>Парк Зеленый Лог</p>
+                            </div>
+                        </div>
+                        <div className='program__time-flex'>
+                            <p className='program__time'>17:00</p>
+                            <p className='program__dash'>—</p>
+                            <div className='program__item-flex'>
+                                <p className='program__margin-zero program__paragraph-up'>Церемония</p>
+                            </div>
+                        </div>
+                        <div className='program__time-flex'>
+                            <p className='program__time'>18:00</p>
+                            <p className='program__dash'>—</p>
+                            <div className='program__item-flex'>
+                                <p className='program__margin-zero program__paragraph-up'>Свадебный ужин</p>
+                                <p className='program__margin-zero program__paragraph-down'>Время танцев, вкусной еды и любви</p>
+                            </div>
+                        </div>
+                        <div className='program__time-flex'>
+                            <p className='program__time'>00:00</p>
+                            <p className='program__dash'>—</p>
+                            <div className='program__item-flex'>
+                                <p className='program__margin-zero program__paragraph-up'>Заверешние вечера</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className='program__time-flex'>
-                    <p className='program__time'>17:00</p>
-                    <p className='program__dash'>—</p>
-                    <div className='program__item-flex'>
-                        <p className='program__margin-zero program__paragraph-up'>Церемония</p>
-                    </div>
-                </div>
-                <div className='program__time-flex'>
-                    <p className='program__time'>18:00</p>
-                    <p className='program__dash'>—</p>
-                    <div className='program__item-flex'>
-                        <p className='program__margin-zero program__paragraph-up'>Свадебный ужин</p>
-                        <p className='program__margin-zero program__paragraph-down'>Время танцев, вкусной еды и любви</p>
-                    </div>
-                </div>
-                <div className='program__time-flex'>
-                    <p className='program__time'>00:00</p>
-                    <p className='program__dash'>—</p>
-                    <div className='program__item-flex'>
-                        <p className='program__margin-zero program__paragraph-up'>Заверешние вечера</p>
-                    </div>
-                </div>
             </div>
             <img className='program__vector-2' src={vector} alt="дуга" />
         </div>
